756d305776a7cbbc5b38e40c3fca8fbc2706876d
BryanRO-SpTech <bryan.grocha@sptech.school>
2024-10-24 15:55:33 -0300

    crop image

--- public/js/register.js
+++ public/js/register.js
@@ -4,6 +4,8 @@ const message = document.querySelector(".welcome h2");
 const previewPhotoDiv = document.getElementById("profile-photo");
 const uploadPhoto = document.getElementById("upload");
 
+let profilePhoto;
+
 function nextPage() {
     page1.style.display = "none";
     page2.style.display = "flex";
@@ -19,15 +21,70 @@ function prevPage() {
 }
 
 
-function previewPhoto() {
+// function previewPhoto() {
+//     const file = uploadPhoto.files[0];
+
+
+//     if (file.type.includes("image")) {
+//         previewPhotoDiv.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+//     } else {
+//         previewPhotoDiv.style.border = `2px solid red`;
+//     }
+// }
+
+
+function previewImage() {
     const file = uploadPhoto.files[0];
 
-    if (file.type.includes("image")) {
-        previewPhotoDiv.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
-    } else {
-        previewPhotoDiv.style.border = `2px solid red`;
+    console.log(file)
+
+    if (!file.type.includes("image")) {
+        previewImage.style.border = "2px solid red";
+
+        return;
     }
+
+    const cropContainer = document.querySelector(".crop-container");
+
+    cropContainer.style.display = "flex";
+
+    const image = document.getElementById('crop-profile');
+
+    const fileUrl = URL.createObjectURL(file);
+
+    image.src = fileUrl;
+
+    const cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 0,
+        dragMode: 'move',
+        movable: true,
+        zoomable: true,
+        scalable: false,
+        rotatable: false,
+        cropBoxResizable: true,
+        background: false
+
+    });
+
+
+    function crop() {
+        const croppedImage = cropper.getCroppedCanvas({
+            with: 300,
+            heigth: 300
+        });
+
+        const croppedImageUrl = croppedImage.toDataURL('image/png');
+
+        previewPhotoDiv.style.backgroundImage = `url(${croppedImageUrl})`;
+
+        cropContainer.style.display = "none";
+    }
+
+    document.getElementById("save-profile").addEventListener("click", crop)
 }
 
-uploadPhoto.addEventListener("change", previewPhoto);
-
+uploadPhoto.addEventListener("click", () => {
+    uploadPhoto.value = "";
+})
+uploadPhoto.addEventListener("change", previewImage);
