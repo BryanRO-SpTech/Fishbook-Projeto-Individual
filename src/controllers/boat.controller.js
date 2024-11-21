3757219102d8116e1dda1029ef4b2d84ea2a85b6
BryanRO-SpTech <bryan.grocha@sptech.school>
2024-11-21 09:48:38 -0300

    boatPhotoFileName

--- src/controllers/boat.controller.js
+++ src/controllers/boat.controller.js
@@ -2,10 +2,12 @@ const boatModel = require('../models/boat.model.js');
 
 const createBoat = async (req, res, next) => {
     const idUser = req.session.id;
-    const { name, dormitory, restroom, maxCapacity, boatImage } = req.body;
+    const { name, dormitory, restroom, maxCapacity } = req.body;
+    const boatPhotoPath = req.file;
+
 
     try {
-        const create = await boatModel.createBoat(idUser, { name, dormitory, restroom, maxCapacity, boatImage });
+        const create = await boatModel.createBoat(idUser, { name, dormitory, restroom, maxCapacity, boatPhotoPath: boatPhotoPath.filename });
 
         return res.status(201).json({
             message: "Boat created successfully",
